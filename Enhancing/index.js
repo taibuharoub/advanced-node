d507cfb4256ccf1b094d949583f1c3918b25bc3b
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-10 12:42:21 +0300

    Block the event loop

--- Enhancing/index.js
+++ Enhancing/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
+//blocking the event loop
+function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration){}
+}
+
 app.get("/", (req, res, next) => {
+    doWork(5000);
     res.send("Hi there");
 })
 
