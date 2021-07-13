a6d4d2c380eef1b448cd5b7798cf1ba9429c8402
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-13 14:50:32 +0300

    Bench marking

---
+++ Enhancing/server.js
@@ -0,0 +1,22 @@
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require("cluster");
+
+if (cluster.isMaster) {
+  cluster.fork();
+} else {
+  const crypto = require("crypto");
+  const express = require("express");
+  const app = express();
+
+  app.get("/", (req, res, next) => {
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("Hi there");
+    });
+  });
+
+  app.get("/fast", (req, res, next) => {
+    res.send("This was fast");
+  });
+
+  app.listen(3000);
+}
