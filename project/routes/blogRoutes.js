dff0fcdca9caeea56856f0ca9c00d1523967393a
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-21 17:29:50 +0300

    Promisfying the get redis function

--- project/routes/blogRoutes.js
+++ project/routes/blogRoutes.js
@@ -1,31 +1,50 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
-module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+module.exports = (app) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    const redis = require("redis");
+    const redisUrl = "redis://127.0.0.1:6379";
+    const client = redis.createClient(redisUrl);
+
+    // promisfying the get function from redis 
+    const util = require("util");
+    client.get = util.promisify(client.get);
+
+    //Do we have any chached data in redis related 
+    // to this query
+    const cachedBlogs = await client.get(req.user.id);
+    console.log(cachedBlogs);
+
+    //if yes, then respond to the request riht away
+    //and return
+
+    //if no, we need to respond to request
+    //update our cache to store data 
+
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
