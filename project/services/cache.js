d9c27d66e911870c4e103094702815692883ce13
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-21 22:00:48 +0300

    Uniqe keys

--- project/services/cache.js
+++ project/services/cache.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
-//to execute extra code on the whenever the exec function
-//runs (add some extra logic to the exec function)
-
-//reference the original exec function
 const exec = mongoose.Query.prototype.exec;
-
-//use normal function not arrow function
 mongoose.Query.prototype.exec = function () {
-    //add some code that will run before any
-    //query is executed by mongoose
     console.log("I'm about to run a Query");
+
+    // this will be refernce to the current querry we 
+    // trying to execute
+    console.log(this.getQuery()); //access to filters
+    console.log(this.mongooseCollection.name); //access to collection name
     return exec.apply(this, arguments);
 }
