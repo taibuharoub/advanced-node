4b872dbdfe35311f0f0c081e779baaad77d98ba6
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-21 22:39:25 +0300

    Cache implementation

--- project/services/cache.js
+++ project/services/cache.js
@@ -6,8 +6,8 @@ const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
-mongoose.Query.prototype.exec = function () {
-    console.log("I'm about to run a Query");
+mongoose.Query.prototype.exec = async function () {
+    /* console.log("I'm about to run a Query");
 
     // this will be refernce to the current querry we 
     // trying to execute
@@ -15,12 +15,23 @@ mongoose.Query.prototype.exec = function () {
     // console.log(this.mongooseCollection.name); //access to collection name
     
     //Object.assign() is used to copy properties from one
-    //object to another
+    //object to another */
+
     const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     }))
 
-    console.log(key);
+    //see if we have a value for "key" in redis
+    const cacheValue = await client.get(key)
 
-    return exec.apply(this, arguments);
+    //if we do, return that
+    if (cacheValue) {
+        console.log(cacheValue);
+    }
+
+    //otherwise, issue the query and store the result in redis
+    //line below will call the original exec function with
+    //whatever arguments we passed to this function
+    const result =  await exec.apply(this, arguments);
+    console.log(result);
 }
