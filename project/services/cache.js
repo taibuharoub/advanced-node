c7c6c66ca4dc1f55ee5f8088882d9b482fd4ccc0
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-21 23:32:53 +0300

    hydrate Arrays

--- project/services/cache.js
+++ project/services/cache.js
@@ -7,7 +7,7 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = async function () {
-    /* console.log("I'm about to run a Query");
+  /* console.log("I'm about to run a Query");
 
     // this will be refernce to the current querry we 
     // trying to execute
@@ -16,18 +16,20 @@ mongoose.Query.prototype.exec = async function () {
     
     //Object.assign() is used to copy properties from one
     //object to another */
-    // this inside of this function is a refernce to the Query 
-    // we are tring to execute 
+  // this inside of this function is a refernce to the Query
+  // we are tring to execute
 
-    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
-        collection: this.mongooseCollection.name
-    }))
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
 
-    //see if we have a value for "key" in redis
-    const cacheValue = await client.get(key)
+  //see if we have a value for "key" in redis
+  const cacheValue = await client.get(key);
 
-    //if we do, return that
-    /* if (cacheValue) {
+  //if we do, return that
+  /* if (cacheValue) {
         console.log(cacheValue);
         //anything from redis will be in json format
         //so we need to parse it before we return it 
@@ -35,29 +37,39 @@ mongoose.Query.prototype.exec = async function () {
         return JSON.parse(cacheValue);
         // return cacheValue;
     } */
-    if (cacheValue) {
-        // this.model is a reference to the modal that 
-        // reperents this query 
-        //this.model is the same as referencing Blog
-       const doc = new this.model(JSON.parse(cacheValue));
+  if (cacheValue) {
+    // this.model is a reference to the modal that
+    // reperents this query
+    //this.model is the same as referencing Blog
+    //below we were just dealing with a single object
+    //const doc = new this.model(JSON.parse(cacheValue));
 
-        // same as saying
-        /* new Blog({
+    //to handle both cases wher we are dealing with an 
+    // array of objects and just a single object 
+
+    // same as saying
+    /* new Blog({
             title: "hi",
             content: "there"
         }) */
 
-        return doc;
-        // return cacheValue;
-    }
+    // return doc;
+    // return cacheValue;
 
-    //otherwise, issue the query and store the result in redis
-    //line below will call the original exec function with
-    //whatever arguments we passed to this function
-    const result =  await exec.apply(this, arguments);
+    const doc = JSON.parse(cacheValue);
+    // Array.isArray(doc) ? its an array : its an object 
+    return Array.isArray(doc)
+    ? doc.map(d => new this.model(d))
+    : new this.model(doc)
+  }
 
-    //make sure to turn result to json before storing it
-    // inside redis
-    client.set(key, JSON.stringify(result)); 
-    return result;
-}
+  //otherwise, issue the query and store the result in redis
+  //line below will call the original exec function with
+  //whatever arguments we passed to this function
+  const result = await exec.apply(this, arguments);
+
+  //make sure to turn result to json before storing it
+  // inside redis
+  client.set(key, JSON.stringify(result));
+  return result;
+};
