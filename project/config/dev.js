1e768b15dfc9d0fa16422e7cb716c436e1cc8ecd
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-23 17:36:12 +0300

    Server configurations

--- project/config/dev.js
+++ project/config/dev.js
@@ -4,4 +4,5 @@ module.exports = {
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://localhost:27017/advanced-node',
   cookieKey: '123123123',
+  redisUrl = "redis://127.0.0.1:6379"
 };
